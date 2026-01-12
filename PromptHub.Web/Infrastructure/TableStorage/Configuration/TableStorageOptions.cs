8e85feb508bd54418a263fa82249db15fdffcb3a
yaras <yaras6@gmail.com>
2026-01-12 22:57:00 +0100

    Cleanup

--- PromptHub.Web/Infrastructure/TableStorage/Configuration/TableStorageOptions.cs
+++ PromptHub.Web/Infrastructure/TableStorage/Configuration/TableStorageOptions.cs
@@ -30,27 +30,7 @@ public sealed class TableStorageOptions
     public string PromptVotesTableName { get; init; } = "PromptVotes";
 
     /// <summary>
-    /// Gets the table name for the tag index.
-    /// </summary>
-    public string TagIndexTableName { get; init; } = "TagIndex";
-
-    /// <summary>
     /// Gets the table name for the public prompts newest index.
     /// </summary>
     public string PublicPromptsNewestIndexTableName { get; init; } = "PublicPromptsNewestIndex";
-
-    /// <summary>
-    /// Gets the table name for the public prompts most-liked index.
-    /// </summary>
-    public string PublicPromptsMostLikedIndexTableName { get; init; } = "PublicPromptsMostLikedIndex";
-
-    /// <summary>
-    /// Gets the table name for the title search index.
-    /// </summary>
-    public string TitleSearchIndexTableName { get; init; } = "TitleSearchIndex";
-
-    /// <summary>
-    /// Gets the table name for the tag catalog.
-    /// </summary>
-    public string TagCatalogTableName { get; init; } = "TagCatalog";
 }
